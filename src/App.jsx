8fedfc3e3c1a6bd3110f58b753bd8be8eb94972a
pulze <malagodap2@gmail.com>
2024-12-11 10:16:28 +0530

    inline style changes added.

--- src/App.jsx
+++ src/App.jsx
@@ -11,3 +11,32 @@ export default function App() {
     </>
   );
 }
+
+
+// function App() {
+//   const [choice, setChoice] = React.useState(null);
+  
+//   let textColor = 'white';
+  
+//   if (choice === 'yes') {
+//       textColor = 'red';
+//   } else if (choice === 'no') {
+//       textColor = 'green';
+//   }
+  
+//   return (
+//   <div id="app">
+//     <h1 style={{ color: textColor }}>CSS is great!</h1>
+//     <menu>
+//       <li>
+//         <button onClick={() => setChoice('yes')}>Yes</button>
+//       </li>
+//       <li>
+//         <button onClick={() => setChoice('no')}>No</button>
+//       </li>
+//     </menu>
+//   </div>
+// );
+// }
+
+// export default App;
